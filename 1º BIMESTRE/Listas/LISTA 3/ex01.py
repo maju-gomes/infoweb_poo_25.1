7d70cb58703e7bc5230c824507f35dfd551ba2c2
maju <julia.gomes1@escolar.ifrn.edu.br>
2025-04-23 20:29:02 +0000

    2 exercícios feitos

---
+++ 1º BIMESTRE/Listas/LISTA 3/ex01.py
@@ -0,0 +1,8 @@
+print("Digite dois valores inteiros: ")
+
+num1 = int(input())
+num2 = int(input())
+
+prod = num1 * num2 
+
+print(f"PROD = {prod}")
---
+++ 1º BIMESTRE/Listas/LISTA 3/ex02.py
@@ -0,0 +1,8 @@
+print("Bem vindo(a) à calculadora de médias!")
+
+notaA = float(input("Nota A: "))
+notaB = float(input("Nota B: "))
+
+media = (notaA * 3.5 + notaB * 7.5) / 11
+
+print(f"MÉDIA: {media:.5f}")
